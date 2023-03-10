2cb721688541ace3e3ff92588cecea27e2d41ca1
mirzaie-mehdi <96014103+mirzaie-mehdi@users.noreply.github.com>
2023-03-10 12:19:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def home():
     '''
       Hello page, shows photos in the /static folder
     '''
-    kittens = Path('/static2/').rglob('org.jpg')
+    kittens = Path('/static/').rglob('*.jpg')
     return flask.render_template_string(
         PAGE,
         student=config["student"],
